14430931218abe2d2e41074de6e6d005ed02db6d
HollowFire <hijackzzb@outlook.com>
2022-07-26 15:26:12 +0800

    refactor calculator to use reflection

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,18 +5,18 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     @Test
-    public void shouldReturn2When1Plus1() {
+    public void shouldReturn2When1Plus1() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResults = calculator.compute(1, 1, '+');
+        int actualResults = calculator.compute(1, 1, "Add");
         int expectedResult = 2;
         Assert.assertEquals(expectedResult, actualResults);
     }
 
 
     @Test
-    public void shouldReturn0When1Sub1() {
+    public void shouldReturn0When1Sub1() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResults = calculator.compute(1, 1, '-');
+        int actualResults = calculator.compute(1, 1, "Sub");
         int expectedResult = 0;
         Assert.assertEquals(expectedResult, actualResults);
     }
